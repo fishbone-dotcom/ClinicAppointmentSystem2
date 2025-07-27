680cb5d3113afe99bc17519de0e8311fe87ebce1
Noel Llosa <noellosa@jeonsoft.com>
2025-07-27 12:25:44 +0800

    fix cant get cookies when production

--- routes/login.js
+++ routes/login.js
@@ -45,8 +45,8 @@ router.post('/login', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
